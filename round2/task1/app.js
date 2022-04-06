b9e72e615abc9afa6359e057eae798b0d6ec7020
14 ghufran almomani <almomanighufran@gmail.com>
2022-04-06 14:31:27 -0600

    round2

---
+++ round2/task1/app.js
@@ -0,0 +1,19 @@
+function palindrome(x){
+    let arry= x.split('');
+let l=arry.length;
+for(let i=0; i<l/2; i++){
+    if (arry[i] == arry[l-1 -i]){
+        continue;
+   
+    }
+    else{
+        return false;
+    }
+
+}
+return true;
+}
+
+console.log(palindrome('ABCDCBA'));
+console.log(palindrome('ghufran'));
+console.log(palindrome('bab'));
