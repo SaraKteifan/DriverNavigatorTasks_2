7f5a95a6536cbf854677bda3eaf72d8f4bbaf0d3
14 ghufran almomani <almomanighufran@gmail.com>
2022-04-06 17:27:59 -0600

    round2

---
+++ round2/task2/app.js
@@ -0,0 +1,21 @@
+function decending(x){
+    let l= x.length;
+    let array= [];
+    let max= x[0];
+    let index= 0;
+    for(let j=0; j<l; j++){
+    for(let i=0; i<l; i++){
+        if(x[i] >= max){
+            max= x[i];
+            index= i;
+        }
+    }
+    array.push(max);
+    x.splice(index,1)
+    max= x[0];
+}
+return array;
+}
+
+console.log(decending([2,9,0,4]));
+console.log(decending([1,2,3,4]));
